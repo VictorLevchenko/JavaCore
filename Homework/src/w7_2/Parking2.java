3b2c1d985680da14efbf940ea1a84b1555167992
VictorLevchenko <victorvladlev@gmail.com>
2016-04-09 23:02:47 +0300

    TreeMap version of Parking

--- Homework/src/w7_2/Parking2.java
+++ Homework/src/w7_2/Parking2.java
@@ -2,22 +2,20 @@ package w7_2;
 
 import java.util.*;
 
-public class Parking {
+public class Parking2 {
 	private final int capacity; //max number of spots in parking
-	public TreeSet<Car> occupied = new TreeSet<>();
+	public TreeMap<Integer, String> occupied = new TreeMap<>();
 	
-	public Parking(int numberOfParkingSpots) {
+	public Parking2(int numberOfParkingSpots) {
 		this.capacity = numberOfParkingSpots;
 		}
 	public int getCapacity() {
 		return capacity;
 	}
 	public boolean checkIn(String s) {
-		Car car = new Car(s);
 		int pN = - 1;
 		if((pN = findEmptySpot()) != - 1) {
-			car.setSpot(pN);
-			occupied.add(car);
+			occupied.put(pN, s);
 			return true;
 		} else {
 			return false;
@@ -29,30 +27,27 @@ public class Parking {
 		if(occupied.size() == capacity) 
 			return - 1;
 		else {
-			
-			Iterator<Car> it = occupied.iterator();
-			while(it.hasNext()) {
-				if (it.next().getSpot() != spot) 
-					break; 
+			for(Integer k : occupied.keySet()) {
+				if (k != spot)
+					break;
 				spot ++;
 			}
 		}
 			return spot;
-			
 	}
 	public String checkOut(int spotNumber) {
-		for(Car c: occupied) {
-			if(c.getSpot() == spotNumber) {
-				occupied.remove(c);
-				return c.getPlate();
+		for(Integer k: occupied.keySet()) {
+			if(k == spotNumber) {
+				occupied.remove(k);
+				return occupied.get(k);
 			}
 		}
 		return null;
 	}
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		for (Car c : occupied)
-			sb.append(c.getPlate() + " - " + c.getSpot() + ", ");
+		for (Integer k : occupied.keySet())
+			sb.append(occupied.get(k) + " - " + k + ", ");
 		return sb.toString();
 	}
 	public int getOccupiedSpace() {
@@ -62,35 +57,9 @@ public class Parking {
 		return (getCapacity() - occupied.size());
 	}
 	
-	private class Car  implements Comparable<Car> {
-		private String plate;
-		private int spot; // spot in parking
-		Car(String number) {
-			setPlate(number);
-		}
-		public String getPlate() {
-			return plate;
-		}
-		public void setPlate(String plate) {
-			this.plate = plate;
-		}
-		public int getSpot() {
-			return spot;
-		}
-		public void setSpot(int spot) {
-			this.spot = spot;
-		}
-		@Override
-		public int compareTo (Car c) {
-			int nSThis = getSpot();
-			int nS = c.getSpot();
-			return (nSThis == nS) ? 0 : (nSThis - nS); 
-		}
-		
-	}
 	public static void main(String[] args) {
 		final int NUM = 10;
-		Parking parking = new Parking(NUM);
+		Parking2 parking = new Parking2(NUM);
 		Random rnd = new Random();
 		for(int i = 0; i < 3; i++) {
 			parking.checkIn("Car"); System.out.println("In *** " + parking);
